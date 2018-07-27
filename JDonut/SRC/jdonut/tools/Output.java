3b754f9bb1e7f100213eb1536e12e34b1b62ffcc
JeremyPace <30711940+JeremyPace@users.noreply.github.com>
2018-07-27 08:29:27 -0600

    Upload Source for version 1.01

---
+++ JDonut/SRC/jdonut/tools/Output.java
@@ -0,0 +1,42 @@
+package jdonut.tools;
+
+public class Output {
+	/**
+	 * Prints arg1 on a new line.
+	 */
+	public static void println(String arg1) {
+		System.out.println(arg1);
+	}
+	/**
+	 * Prints arg1 on a new line.
+	 */
+	public static void println(int arg1) {
+		System.out.println(arg1);
+	}
+	/**
+	 * Prints arg1 on a new line.
+	 */
+	public static void println(boolean arg1) {
+		System.out.println(arg1);
+	}
+	
+	/**
+	 * prints arg1.
+	 */
+	public static void print(String arg1) {
+		System.out.print(arg1);
+	}
+	/**
+	 * Prints arg1.
+	 */
+	public static void print(int arg1) {
+		System.out.print(arg1);
+	}
+	/**
+	 * Prints arg1.
+	 */
+	public static void print(boolean arg1) {
+		System.out.print(arg1);
+	}
+	
+}
